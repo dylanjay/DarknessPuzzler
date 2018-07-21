e9dd38a6615277265153953506463e7bde943a18
cdeng001 <cdeng001@ucr.edu>
2018-07-21 16:17:36 -0400

    add shake script

---
+++ Assets/Scripts/Gameplay/Management/CameraShake.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    public float maxAngle = 5;
+    public float maxOffset = 1;
+
+    //initial values
+    bool shaking;
+    Vector3 _basePos;
+    Vector3 _baseRot;
+    
+    float trauma = 0;
+
+	// Use this for initialization
+	void Start () {
+        shaking = false;
+        Random.InitState( (int)System.DateTime.Now.Ticks );
+
+        _basePos = transform.localPosition;
+        _baseRot = transform.localEulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(trauma > 0)
+        {
+            getShakeTranslation();
+            trauma -= 0.01f;
+        }
+        else
+        {
+            resetCameraPosition();
+        }
+	}
+
+    void resetCameraPosition()
+    {
+        transform.localPosition = _basePos;
+        transform.localEulerAngles = _baseRot;
+    }
+    
+    void getShakeTranslation()
+    {
+        float shake = Mathf.Pow(trauma, 3.0f);
+        float offsetX = maxOffset * shake * Random.value;
+        float offsetY = maxOffset * shake * Random.value;
+        float angle = maxAngle * shake * Random.value;
+        
+        Vector3 newEulerAngle = new Vector3(_baseRot.x, _baseRot.y, _baseRot.z + angle);
+        Vector3 newPosition =  new Vector3(_basePos.x + offsetX, _basePos.y + offsetY, _basePos.z);
+        transform.localEulerAngles = newEulerAngle;
+        transform.localPosition = newPosition;
+    }
+
+    public void shakeCamera(float t)
+    {
+        trauma = t;
+        if (trauma > 1)
+        {
+            trauma = 1;
+        }
+    }
+}
--- Assets/Scripts/Gameplay/Player/PlayerKiller.cs
+++ Assets/Scripts/Gameplay/Player/PlayerKiller.cs
@@ -5,14 +5,10 @@ public class PlayerKiller : MonoBehaviour
 {
     PlayerRespawner respawner;
     DeadbodyManager deadbodyManager;
-    public float shakeAmt = 0;
-    Camera mainCamera;
-    Vector3 originalCameraPosition;
 
     void Awake()
     {
         respawner = GetComponent<PlayerRespawner>();
-        mainCamera = Camera.main;
     }
 
     void Start()
@@ -23,33 +19,13 @@ public class PlayerKiller : MonoBehaviour
     public void Kill()
     {
         deadbodyManager.CreateBody(transform);
-        originalCameraPosition = mainCamera.transform.position;
-        InvokeRepeating("CameraShake", 0, .01f);
-        Invoke("StopShaking", 0.3f);
-        StartCoroutine(RespawnRoutine());
+        Camera.main.GetComponent<CameraShake>().shakeCamera(1);
     }
 
     IEnumerator RespawnRoutine()
     {
         yield return new WaitForEndOfFrame();
-
         respawner.Respawn();
     }
 
-    void CameraShake()
-    {
-        if (shakeAmt > 0)
-        {
-            float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
-            Vector3 pp = mainCamera.transform.position;
-            pp.y += quakeAmt; // can also add to x and/or z
-            mainCamera.transform.position = pp;
-        }
-    }
-
-    void StopShaking()
-    {
-        CancelInvoke("CameraShake");
-        mainCamera.transform.position = originalCameraPosition;
-    }
 }
