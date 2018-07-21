a29a8d75d793a5f190d0df38a96b3582a20f75ee
cdeng001 <cdeng001@ucr.edu>
2018-07-20 23:58:19 -0400

    adding projectile

---
+++ Assets/Scripts/Gameplay/Projectile/ProjectileScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    SpriteRenderer sprite;
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.layer != LayerMask.NameToLayer("Player")){
+            Debug.Log(col.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
