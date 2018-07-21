72a7f9da003dccd6928a197e8c7d3644934d2bf9
dylan <dylanjay9@gmail.com>
2018-07-21 13:11:54 -0700

    Conveyor belt works on dead bodies

--- Assets/Scripts/Gameplay/World/ConveyorBelt.cs
+++ Assets/Scripts/Gameplay/World/ConveyorBelt.cs
@@ -5,11 +5,21 @@ public class ConveyorBelt : MonoBehaviour
     public bool goingRight = true;
     public float speed;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    //void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Pickupable")
+    //    {
+    //        collision.GetComponent<Rigidbody2D>().AddForce(speed * (goingRight ? Vector3.right : Vector3.left));
+    //    }
+    //}
+
+    void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Pickupable")
         {
-            collision.GetComponent<Rigidbody2D>().AddForce(speed * (goingRight ? Vector3.right : Vector3.left));
+            // TODO Set constant speed by adding velocity here and to player
+            // TODO fix pickup collider activating conveyor belt
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(speed * (goingRight ? Vector3.right : Vector3.left));
         }
     }
 }
