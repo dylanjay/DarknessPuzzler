6999cf32e80fae515386dd582a8b93df25d13ce9
Mark De Ruyter <mderu001@ucr.edu>
2018-07-21 21:08:32 -0700

    Dead Body Rotation Fixes

---
+++ Assets/DeadBody.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DeadBody : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        spriteRenderer.flipY = Physics2D.gravity.y < 0;
+        transform.rotation = Quaternion.identity;
+    }
+}
